8351885a880cb69101405cffe94300c9c4e38492
UBC Student <aliyam@valdes.students.cs.ubc.ca>
2021-02-28 23:44:24 -0800

    decoding responses, implement seperate functions based on record type next

--- src/ca/ubc/cs317/dnslookup/processing/DNSByteResults.java
+++ src/ca/ubc/cs317/dnslookup/processing/DNSByteResults.java
@@ -32,6 +32,16 @@ public class DNSByteResults {
     arcount = decodeBytesToInt(10, 11);
     int ind = decodeQuestion(12);
     System.out.println("last index is: " + ind);
+    // TODO: if ans available return ans, else can return 0.0.0.0 as the IP
+
+    // decode each record
+    // int totals = ancount + nscount + arcount;
+    // while (totals > 0) {
+    //   decodeRecord(ind);
+    //   totals--;
+    // }
+
+    decodeRecord(ind);
   }
 
   // dig +norecurse @199.7.83.42 finance.google.ca --> part1
@@ -87,8 +97,102 @@ public class DNSByteResults {
     int qtype = decodeBytesToInt(startIndex, ++startIndex);
     System.out.println("q type: " + qtype);
     node = new DNSNode(sb.toString(), RecordType.getByCode(qtype));
-    // TODO: qclass?
+    startIndex++;
+    
+    //TODO store qclass?
+    System.out.println(startIndex);
+    int qclass = decodeBytesToInt(startIndex, ++startIndex);
+    System.out.println("q class: " + qclass);
+
+    // check pointer next or 
     return ++startIndex;
+  }
+  private String getNameAtOffset(int startIndex) {
+    byte length = 0;
+    StringBuilder sb = new StringBuilder();
+    do {
+      length = resultArray[startIndex++]; // 12
+      if (length == 0)
+        break;
+      byte[] domain = new byte[length];
+      for (int i = 0; i < (int) length; i++) {
+        domain[i] = resultArray[startIndex++];
+      }
+      String domainName;
+      try {
+        domainName = new String(domain, "US-ASCII");
+        sb.append(domainName);
+        sb.append(".");
+      } catch (UnsupportedEncodingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } while (length != 0);
+    sb = sb.deleteCharAt(sb.length()-1);
+    System.out.println("domain name: " + sb.toString());
+    return sb.toString();
+  }
+  private int decodeRecord(int startIndex){
+    // check for message compression - if first byte 11 (17 in int conversion)
+    int firstByteAsInt = resultArray[startIndex];
+    int shifted = resultArray[startIndex] >> 6;
+    System.out.println(shifted);
+    // 11 in binary is equivalent to -1 because int datatype is signed by default
+    if (shifted == -1) {
+      System.out.print(decodeBytesToInt(startIndex, startIndex+1));
+      int offset = decodeBytesToInt(startIndex, startIndex+1) & 0x3fff;
+      System.out.println(offset);
+      String name = getNameAtOffset(offset);
+      startIndex = startIndex + 2;
+    } else {
+      String name = getNameAtOffset(startIndex);
+      // TODO how to increment startIndex in this case, maybe startIndex should be global
+    }
+    RecordType type = RecordType.getByCode(decodeBytesToInt(startIndex, ++startIndex));
+    startIndex++;
+    int rclass = decodeBytesToInt(startIndex, ++startIndex);
+    startIndex++;
+    System.out.println("index before ttl grab " + startIndex);
+    byte[] ttlBytes = Arrays.copyOfRange(resultArray, startIndex, startIndex+4);
+    int ttl = ByteBuffer.wrap(ttlBytes).getInt();
+    System.out.println("ttlbytes length: " + ttlBytes.length);
+    System.out.println("ttl: " + ttl);
+    startIndex = startIndex + 4;
+    int rlength = decodeBytesToInt(startIndex, ++startIndex);
+    startIndex++;
+
+    String result = getRecordResultBasedOnRecordType(startIndex, rlength, type, rclass);
+    // byte shifted = (byte)((int) resultArray[startIndex]) >> 6);
+    // System.out.print(String.format("0x%02X", shifted) + ", ");
+
+    // if (firstByteAsInt == 17) {
+    //   System.out.println("message compression");
+    //   // c0 00 
+    //   int offset = resultArray[startIndex++];
+    // }
+    return startIndex;
+  }
+
+//TODO
+  private String getRecordResultBasedOnRecordType(int startIndex, int rlength, RecordType type, int rclass){
+    int typeCode = type.getCode();
+    switch(typeCode) {
+      case 1:
+        System.out.println("Type A record");
+        break;
+      case 2:
+        System.out.println("Type NS record");
+        break;
+      case 5:
+        System.out.println("Type CNAME record");
+        break;
+      case 28:
+        System.out.println("Type AAAA record");
+        break;
+      default:
+        return "";
+    }
+    return "";
   }
 
 }
